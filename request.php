b07a248933365ff4c46420442c27dd3e047518f6
Raghad <raghad444979@gmail.com>
2022-11-06 18:28:14 +0300

    jbnnk

---
+++ request.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta chrset="utf-8">
+   <title>Request</title>
+<link rel="stylesheet" type="text/css" href="styleee.css">
+
+<script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" type="image/x-icon" href=KG1.png>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+
+<body>
+<nav>
+        <div class="conatainer">
+            <img src="KG4.png" class="logo" alt="Logo" />
+    
+              
+                <ul>
+                    <li><a href="Home.html">Home</a></li>
+                    <li><a href="#footer">About us</a></li>
+                    <li><a href="Home.html">Sign out</a></li>
+                </ul>
+        </div>
+         </nav>
+
+         
+         <div class="set">
+            <h1 class="offer">Request Details</h1>
+ <p>
+ <strong>Requested by: </strong>Mrs. Iman. <br>
+ <strong>kid/s Name:</strong>Nora . <br>
+ <strong>kid/s Age:</strong> 3 years.<br>
+ <strong>type of service:</strong>
+ <ul><li>Infant babysitting.</li> <li>Helping the baby eat his meals</li></ul>
+ <strong>duration:</strong> 2 hours.
+ </p>
+ <p>
+<label><strong>Send offer(Price)</strong><input type="text"  name="Accept"></label><br>
+<a href="request List.html"><button class="buttons">Send </button> </a>
+
+<a href="request List.html"><button class="buttons">Cancel </button> </a>
+</p></div>
+
+
+
+
+
+
+
+
+<div class= "footer">
+    <div class="col-1">
+        <h3><b>ABOUT US</b></h3>
+         
+        <p>At KinderGuardians we distinguish ourselves on creating special 
+            memorable moments for your little angels. Our babysitters don't just babysit your kids, 
+            they teach them valuable lessons that keep them amused and more importantly smiling thanks
+             to their creative teaching style.</p>
+      </div><br>
+  <div id="footer"></div>
+  <div class="col-2">
+  
+  <h3><b>NEWSLETTER</b></h3>
+  <form>
+  
+    <input type="text" placeholder="Your Email Address" required>
+    <br>
+    <button type="submit">SUBSCRIBE NOW </button>
+  </form>
+  </div><br>
+  
+  <div class="col-3">
+  <h3><b>CONTACT US</b></h3>
+  <a href="mailto:info@kinderguardians.com">info@kinderguardians.com</a>
+  <div class="social-icons"></div>
+  <i class="fab fa-twitter"></i>
+  <i class="fab fa-instagram"></i>
+  <br><br><br>
+  <p>&copy;2022 KinderGuardians</p>
+  </div>
+  </div>
+</body>
+</html>
